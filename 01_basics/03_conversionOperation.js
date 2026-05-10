89287958ae32e35a24acec6441c9c3fcd7e8497d
SudhanshuShekharrr <148934036+SudhanshuShekharrr@users.noreply.github.com>
2026-05-10 17:15:24 +0530

    Add 01_basics/03_conversionOperation.js

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,12 @@
+let score = "33"
+
+console.log(typeof score);
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+// "33" -> 33
+// "45abc" -> NaN (not a number)
+// true -> 1, false -> 0
+// 1 -> true, 0 -> false
+// "" -> false
+// "Sudhanshu" -> true;
