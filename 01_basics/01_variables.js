d7e35b1cc63fbde903a6ce0085f8e3a06508514b
SudhanshuShekharrr <148934036+SudhanshuShekharrr@users.noreply.github.com>
2026-05-10 16:54:10 +0530

    first commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 1234;
+let accountEmail = "sudhanshu@google.com"
+let accountPassword = "1234"
+let accountCity = "Madhubani"
+accountPassword = "1122"
+console.table([accountId,accountEmail,accountPassword, accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+let name = "SWudhanshu"  // this is string data type
+let age = 20 // this is integer data type
+let isLogged = false // this is boolean type of data
+
+// number goes upto 2 pow 53
+// bigint used to very large data
+
+// boolean : true/false 
+
+// null : it is an intentional empty value, type -> object
+
+// undefined : variable exist but has no value assigned yet  type -> undefined
+
+// symbol : unique
+
+// object 
+
+
+
