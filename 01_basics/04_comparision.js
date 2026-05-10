444571ef6b77c8e639655ba6905b6337fa599695
SudhanshuShekharrr <148934036+SudhanshuShekharrr@users.noreply.github.com>
2026-05-10 17:57:15 +0530

    add 01_basics/04_comparision.js

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,11 @@
+console.log("2" > 1) // True
+console.log("02" > 1) // True
+
+console.log(null > 0); // false
+console.log(null == 0); // false 
+console.log(null >= 0); // true
+
+
+console.log("2" ==2) // true
+
+console.log("2" === 2) // false -> it gave false because we used  ===  it also check datatype along with number ;
