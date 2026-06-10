f00bf6606dc3a81d7235dfdd2beded3624e3f088
SudhanshuShekharrr <148934036+SudhanshuShekharrr@users.noreply.github.com>
2026-06-10 16:29:50 +0530

    08_Projects/02_project.js

---
+++ 08_Projects/02_project.js
@@ -0,0 +1,6 @@
+const parent = document.getElementById('parent');
+parent.addEventListener('click',(e)=>{
+    // console.log(e.target);
+    const child = e.target;
+    document.body.style.backgroundColor = child.id;
+})
